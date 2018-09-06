e8eaf31eb227cd2c8fe3c2c0b283d3f888028db7
Revilo1882 <oliverrharris@gmail.com>
2018-09-06 12:18:49 +0100

    Use redux and react-redux with actions and reducers

--- src/components/Stack.js
+++ src/components/Stack.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 class Stack extends Component {
   render() {
+    const { title, cards } = this.props.stack;
+
+    console.log('stack props', this.props)
     return (
       <div>
         <Link to='/'>Home</Link>
-        <h3>example title</h3>
+        <h3>{title}</h3>
+        <br />
+        {
+          cards.map(card => {
+            return (
+              <div key={card.id}>
+                {card.prompt}
+              </div>
+            )
+          })
+        }
       </div>
     )
   }
 }
 
-export default Stack;
+function mapStateToProps(state) {
+  return { stack: state }
+}
+
+export default connect(mapStateToProps, null)(Stack);
